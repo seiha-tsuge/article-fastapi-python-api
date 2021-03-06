e316ccc1c484fec7a057684db755f671f987e730
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-03-07 03:38:09 +0900

    faet: use caseの作成

---
+++ src/tests/use_cases/test_user_list.py
@@ -0,0 +1,35 @@
+from unittest import mock
+
+import pytest
+from app.domain.user import User
+from app.use_cases.user_list import user_list_use_case
+
+
+@pytest.fixture
+def domain_users():
+    user_1 = User(
+        id=1,
+        name="name1",
+    )
+
+    user_2 = User(
+        id=2,
+        name="name2",
+    )
+
+    user_3 = User(
+        id=3,
+        name="name3",
+    )
+
+    return [user_1, user_2, user_3]
+
+
+def test_user_list_without_parameters(domain_users):
+    repo = mock.Mock()
+    repo.list.return_value = domain_users
+
+    result = user_list_use_case(repo)
+
+    repo.list.assert_called_with()
+    assert result == domain_users
