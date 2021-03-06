83bd2e62052e2d7238bf8d88ff43ff9e959f35e2
seiha-tsuge <seiha.tsuge@e-tsuge.com>
2021-03-07 03:32:20 +0900

    feat: domainの作成

--- src/app/domain/user.py
+++ src/app/domain/user.py
@@ -1,7 +1,14 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass
 
 
 @dataclass(frozen=True)
 class User:
     id: int
     name: str
+
+    @classmethod
+    def from_dict(cls, d):
+        return cls(**d)
+
+    def to_dict(self):
+        return asdict(self)
--- src/tests/domain/test_user.py
+++ src/tests/domain/test_user.py
@@ -9,3 +9,38 @@ def test_user_model_init():
 
     assert user.id == 1
     assert user.name == "name"
+
+
+def test_user_model_from_dict():
+    init_dict = {
+        "id": 1,
+        "name": "name",
+    }
+
+    room = User.from_dict(init_dict)
+
+    assert room.id == 1
+    assert room.name == "name"
+
+
+def test_user_model_to_dict():
+    init_dict = {
+        "id": 1,
+        "name": "name",
+    }
+
+    room = User.from_dict(init_dict)
+
+    assert room.to_dict() == init_dict
+
+
+def test_user_model_comparison():
+    init_dict = {
+        "id": 1,
+        "name": "name",
+    }
+
+    room1 = User.from_dict(init_dict)
+    room2 = User.from_dict(init_dict)
+
+    assert room1 == room2
